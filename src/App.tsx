909a571103410094cee522b542c1fa59e5785e7f
Alejandro M. Coca <alxmcr@outlook.com>
2021-10-31 03:07:24 -0400

    Integrate Redux, Contexts, and styles in our app

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,29 @@
-import "./App.scss";
+import { BrowserRouter as Router } from "react-router-dom";
+import { AppFooter } from "./components/common/AppFooter";
+import { AppHeader } from "./components/common/AppHeader";
+import { AppRouting } from "./routes/AppRouting";
+import { OverlayProvider } from "./providers/OverlayProvider";
+import { SidebarProvider } from "./providers/SidebarProvider";
 import { AppReduxProvider } from "./providers/AppReduxProvider";
+import "./App.scss";
+import { CitiesProvider } from "./providers/CityProvider";
 
 function App() {
   return (
     <AppReduxProvider>
-      <div className="App">
-        <h1>Bookifly</h1>
-      </div>
+      <CitiesProvider>
+        <OverlayProvider>
+          <SidebarProvider>
+            <Router>
+              <div className="App">
+                <AppHeader />
+                <AppRouting />
+                <AppFooter />
+              </div>
+            </Router>
+          </SidebarProvider>
+        </OverlayProvider>
+      </CitiesProvider>
     </AppReduxProvider>
   );
 }
